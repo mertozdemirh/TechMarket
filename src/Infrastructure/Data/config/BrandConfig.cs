3b0ee5ae157178e24ecd4c49becdc67ad2e65c1d
mert özdemir <mertozdemir91@hotmail.com>
2021-12-20 13:25:55 +0300

    MarketContext

---
+++ src/Infrastructure/Data/config/BrandConfig.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.config
+{
+    public class BrandConfig : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+        }
+    }
+}
